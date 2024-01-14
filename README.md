5ce037432a51382875598fe01d411f06d062cafe
GustavoCesarSantos <gustavocs789@gmail.com>
2024-01-14 16:35:56 -0300

    feat: adiciona swagger

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # read-manager-main-api
-docker container run -p 3000:3000 -e PORT=":3000" -e ENV="teste" -e VERSION=1 teste:3
--- internal/infra/http/routes.go
+++ internal/infra/http/routes.go
@@ -2,9 +2,13 @@ package infra
 
 import (
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+	_ "github.com/swaggo/echo-swagger/example/docs"
+
 	health "github.com/read-manager/read-manager-main-api/internal/health/handlers"
 )
 
 func SetRoutes(e *echo.Echo) {
+	e.GET("/v1/docs/*", echoSwagger.WrapHandler)
 	e.GET("/v1/healthcheck", health.CheckHandler)
 }
